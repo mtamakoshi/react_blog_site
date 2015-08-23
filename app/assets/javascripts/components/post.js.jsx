ef7636cdb397bac5f8fbaf2eab40f67d605b89fb
Mai Tamakoshi <mai23coco@gmail.com>
2015-08-23 11:26:37 -0400

    just for save

--- app/assets/javascripts/components/post.js.jsx
+++ app/assets/javascripts/components/post.js.jsx
@@ -2,7 +2,7 @@ var PostDetail = React.createClass({
 	render: function() {
 		return (
 			<div>
-				<img src="../assets/images/photo_file_name" />
+				<p>{this.props.post.photo}</p>
 				<h2>{this.props.post.title}</h2>
 				<p>hi on {this.props.post.created_at}</p>
 				<p>{this.props.post.body}</p>
--- app/assets/javascripts/components/posts.js.jsx
+++ app/assets/javascripts/components/posts.js.jsx
@@ -2,7 +2,7 @@ var Post = React.createClass({
 	render: function() {
 		return (
 			<div className="post">
-				<img src="../assets/images/this.props.post.photo_file_name" />
+				<p>{this.props.photo}</p>
 				<h3>{this.props.title}</h3>
 				<p>Written on {this.props.created_at}</p>
 				<p className="body">{this.props.body}</p>
@@ -15,7 +15,7 @@ var PostList = React.createClass({
 	render: function() {
 		var postNodes = this.props.posts.map(function (post) {
 			return (
-				<Post title={post.title} created_at={post.created_at} body={post.body} id={post.id} key={post.id} />
+				<Post title={post.title} created_at={post.created_at} body={post.body} photo={post.photo} id={post.id} key={post.id} />
 			)
 		});
 		return (
